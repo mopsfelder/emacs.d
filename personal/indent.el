3e344a7987950bad78a1dd6c40b99a6feb520bfb
Murilo Opsfelder Araujo <mopsfelder@gmail.com>
2014-03-24 18:39:46 -0300

    Adjust indentation
    Use 4-spaces tab in python and sh modes.

--- personal/indent.el
+++ personal/indent.el
@@ -2,15 +2,24 @@
 (defun my-code-indentation ()
   (c-set-offset 'substatement-open 0)
 
+  ; c
   (setq-default c-default-style "linux")
   (setq-default c-block-comment-prefix "* ")
   (setq-default c-basic-offset 8)
   (setq-default c-indent-level 8)
 
-  (setq-default sh-basic-offset 8)
+  ; sh
+  (setq-default sh-basic-offset 4)
+  (setq-default sh-indent-level 4)
 
+  ; tcl
   (setq-default tcl-indent-level 8)
 
+  ; python
+  (setq-default python-basic-offset 4)
+  (setq-default python-indent-level 4)
+
+  ; tab default
   (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
   (setq tab-width 8)
   (setq indent-tabs-mode t))
