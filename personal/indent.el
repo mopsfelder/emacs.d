855421e5b3d3cc9be8866d6394cdf38f8520d46f
Murilo Opsfelder Araujo <mopsfelder@gmail.com>
2014-03-31 21:58:33 -0300

    Use lambda functions for setting indentation

--- personal/indent.el
+++ personal/indent.el
@@ -1,31 +1,25 @@
-;; indentation settings
-(defun my-code-indentation ()
-  (c-set-offset 'substatement-open 0)
+;; c
+(add-hook 'c-mode-hook
+	  (lambda ()
+	    (setq c-default-style "linux"
+		  c-block-comment-prefix "* "
+		  indent-tabs-mode t
+		  c-basic-offset 8
+		  c-indent 8)))
 
-  ; c
-  (setq-default c-default-style "linux")
-  (setq-default c-block-comment-prefix "* ")
-  (setq-default c-basic-offset 8)
-  (setq-default c-indent-level 8)
+;; python
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode nil
+		  python-basic-offset 4
+		  python-indent 4)))
 
-  ; sh
-  (setq-default sh-basic-offset 4)
-  (setq-default sh-indent-level 4)
-
-  ; tcl
-  (setq-default tcl-indent-level 8)
-
-  ; python
-  (setq-default python-basic-offset 4)
-  (setq-default python-indent-level 4)
-
-  ; tab default
-  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
-  (setq tab-width 8)
-  (setq indent-tabs-mode t))
-
-;; set indentation only when in prog-mode
-(add-hook 'prog-mode-hook 'my-code-indentation)
+;; sh
+(add-hook 'sh-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode nil
+		  sh-basic-offset 4
+		  sh-indent 4)))
 
 ;; auto insert close bracket pairs, see http://ergoemacs.org/emacs/emacs_insert_brackets_by_pair.html
 (electric-pair-mode 1)
