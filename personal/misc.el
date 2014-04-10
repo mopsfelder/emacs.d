636fd0d81095726685c97cd9024af109f70bfe04
Murilo Opsfelder Araujo <mopsfelder@gmail.com>
2014-04-10 11:25:28 -0300

    Make scrolling less jumpy

--- personal/misc.el
+++ personal/misc.el
@@ -3,3 +3,13 @@
 
 ;; enable deadkeys on graphical mode, see https://bugzilla.redhat.com/show_bug.cgi?id=224626
 (require 'iso-transl)
+
+;; scroll one line at a time (less "jumpy" than defaults)
+;; http://www.emacswiki.org/emacs/SmoothScrolling
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-follow-mouse 't)
+(setq scroll-margin 5)
+(setq scroll-step 1)
+(setq hscroll-margin 10)
+(setq hscroll-step 1)
