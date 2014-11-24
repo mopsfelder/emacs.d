116a0766da97fad260c9ce8b6c8acdcb32110b91
Murilo Opsfelder Araujo <mopsfelder@gmail.com>
2014-11-24 19:37:30 -0200

    Set right margin line at column 80

--- personal/appearance.el
+++ personal/appearance.el
@@ -31,6 +31,7 @@
 ;; highlight right margin limit
 (require 'fill-column-indicator)
 (setq fci-rule-width 1)
+(setq fci-rule-column 80)
 (add-hook 'after-change-major-mode-hook 'fci-mode)
 
 ;; set font
