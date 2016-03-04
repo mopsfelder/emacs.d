12a4e3350e7087c6e86948d4337e2f95faa75d0b
Murilo Opsfelder Araujo <mopsfelder@gmail.com>
2016-03-04 08:52:55 -0300

    Move personal/preload/packages.el configuration to init.el
    When Emacs 25 starts, it always complains that package initialization
    must come before any other configuration, and it also adds the
    following configuration at the very beginning of the init.el file:
    
        (package-initialize)
    
    For convenience, I moved packages.el configuration to init.el so Emacs
    25 does not complain anymore.
    
    I also removed personal-preload-dir configuration since the
    personal/preload/ directory only had packages.el and it is now empty.

--- init.el
+++ init.el
@@ -1,15 +1,32 @@
+;; This must come before configuration of installed packages.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; list of packages to be installed
+(defvar my-packages '(auto-complete
+		      fill-column-indicator
+		      helm
+		      magit
+                      yaml-mode
+		      zenburn-theme))
+
+;; install packages if not installed yet
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 ;; configuration directory, say ~/.emacs.d
 (defvar conf-dir (file-name-directory load-file-name))
 
 ;; directory housing configuration files to be loaded
 (defvar personal-dir (expand-file-name "personal" conf-dir))
 
-;; directory housing files to be loaded before others
-(defvar personal-preload-dir (expand-file-name "preload" personal-dir))
-
 ;; directory housing saveplaces
 (defvar savefile-dir (expand-file-name "saves" conf-dir))
 
 ;; load personal settings
-(mapc 'load (directory-files personal-preload-dir 't "^[^#].*el$"))
 (mapc 'load (directory-files personal-dir 't "^[^#].*el$"))
